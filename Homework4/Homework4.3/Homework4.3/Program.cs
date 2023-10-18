08c98b24c43374a7b63eab92592eacf170297ffb
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-10-18 13:27:07 +0300

    Add new solution 4.3

--- Homework4/Homework4.3/Homework4.3/Program.cs
+++ Homework4/Homework4.3/Homework4.3/Program.cs
@@ -2,53 +2,57 @@
 bool isParceSuccess = int.TryParse(Console.ReadLine(), out int arrayLength);
 if (isParceSuccess && arrayLength > 2)
 {
-    int[] numbers = new int[arrayLength]; for (int i = 0; i < arrayLength; i++)
+    int[] numbers = new int[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
     {
-        Console.Write($"Введите {i + 1} элемент массива: "); numbers[i] = int.Parse(Console.ReadLine());
+        Console.Write($"Введите {i + 1} элемент массива: ");
+        numbers[i] = int.Parse(Console.ReadLine());
     }
-    int[] duplicateCount = new int[arrayLength];
-    string[] duplicateNumbers = new string[arrayLength];
-    for (int i = 0; i < arrayLength; ++i)
+    int temp = 0;
+    for (int i = 0; i < arrayLength; i++)
     {
-        int count = 0;
-        bool isCopy = false;
-        for (int l = 0; l < arrayLength; ++l)
+        for (int j = i + 1; j < arrayLength; j++)
         {
-            if (numbers[i].ToString() == duplicateNumbers[l])
+            if (numbers[j] < numbers[i])
             {
-                isCopy = true;
-                break;
+                temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
             }
         }
-
-        for (int l = 0; l < arrayLength; ++l)
+    }
+    for (int i = 0; i < arrayLength; i++)
+    {
+        Console.Write($"{numbers[i]}  ");
+    }
+    Console.WriteLine();
+    string name = null;
+    for (int i = 0; i < arrayLength; i++)
+    {
+        string numbersString = numbers[i].ToString();
+        if (numbersString == name)
         {
-            if (numbers[i] == numbers[l] && isCopy == false)
+            continue;
+        }
+        else
+        {
+            name = numbersString;
+            int count = 0;
+            for (int j = 0; j < arrayLength; j++)
             {
-                ++count;
+                if ((numbers[j] == numbers[i]))
+                {
+                    count++;
+                }
+            }
+            if (count > 1)
+            {
+                Console.WriteLine($"Число {numbers[i]} повторяется {count} раз(а) ");
             }
         }
-        if (count > 1)
-        {
-            duplicateCount[i] = count;
-            duplicateNumbers[i] = numbers[i].ToString();
-        }
-    }
-    bool flag = false;
-    for (int i = 0; i < arrayLength; ++i)
-    {
-        if (duplicateCount[i] > 0)
-        {
-            Console.WriteLine($"Элемент {duplicateNumbers[i]} повторяется {duplicateCount[i]} раз");
-            flag = true;
-        }
-    }
-    if (flag == false)
-    {
-        Console.WriteLine("Все элементы уникальны!");
     }
 }
 else
 {
-    Console.WriteLine("Длина массива введена некорректно!");
+    Console.Write("Длина массива введена некорректно!");
 }
