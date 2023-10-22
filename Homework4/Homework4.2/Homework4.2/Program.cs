4fdb113b22ca604b2622d3a387dc431852f633be
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-10-22 21:48:06 +0300

    Add double loop fix

---
+++ Homework4/Homework4.2/Homework4.2/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите количество элементов массива (>2): ");
+bool isParceSuccess = int.TryParse(Console.ReadLine(), out int arrayLength);
+if (isParceSuccess && arrayLength > 2)
+{
+    int[] numbers = new int[arrayLength];
+    for (int i = 0; i < arrayLength; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} элемент массива:");
+        numbers[i] = int.Parse(Console.ReadLine());
+    }
+    int max = numbers[0];
+    int min = numbers[0];
+    for (int i = 1; i < arrayLength; i++)
+    {
+        if (numbers[i] > max)
+        {
+            max = numbers[i];
+        }
+        else
+        {
+            min = numbers[i];
+        }
+    }
+    Console.WriteLine($"элемент {max} - максимальный");
+    Console.WriteLine($"элемент {min} - минимальный");
+    int numberDifference = max - min;
+    Console.WriteLine($"Разность максимума и минимума = {numberDifference}");
+}
+else
+{
+    Console.WriteLine("Длина массива введена некорректно!");
+}
