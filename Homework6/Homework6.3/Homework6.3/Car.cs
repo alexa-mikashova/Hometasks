c476814c0beff3e9ec2982942ffceb8c090eda0c
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-10-27 19:43:52 +0300

    Add Homework6.3

---
+++ Homework6/Homework6.3/Homework6.3/Car.cs
@@ -0,0 +1,39 @@
+﻿namespace Homework6._3
+{
+    public class Car
+    {
+        private string _color;
+        private string _type;
+        private int _year;
+        public Car(string color, string type, int year)
+        {
+            _color = color;
+            _type = type;
+            _year = year;
+        }
+        public void CarStarting()
+        {
+            Console.WriteLine("Автомобиль заведён");
+        }
+        public void TurningOffTheCar()
+        {
+            Console.WriteLine("Автомобиль заглушён");
+        }
+        public void CarColor()
+        {
+            _color = "черного";
+        }
+        public void CarType()
+        {
+            _type = "BMW";
+        }
+        public void CarYear()
+        {
+            _year = 2006;
+        }
+        public void CarInformation()
+        {
+            Console.WriteLine($"Автомобиль {_type} {_color} цвета {_year} года выпуска.");
+        }
+    }
+}
---
+++ Homework6/Homework6.3/Homework6.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using Homework6._3;
+using System.Globalization;
+using static System.Net.Mime.MediaTypeNames;
+Console.Write("Введите марку автомобиля: ");
+string type = Console.ReadLine();
+Console.Write("Введите цвет автомобиля: ");
+string color = Console.ReadLine();
+bool isParceSuccessOne;
+int year;
+do
+{
+    Console.Write("Введите год выпуска автомобиля: ");
+    isParceSuccessOne = int.TryParse(Console.ReadLine(), out year);
+}
+while (!isParceSuccessOne || year < 1900 || year > 2023);
+Car car = new(type, color, year);
+car.CarStarting();
+car.TurningOffTheCar();
+car.CarType();
+car.CarColor();
+car.CarYear();
+car.CarInformation();
