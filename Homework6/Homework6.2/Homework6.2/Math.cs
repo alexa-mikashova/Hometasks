55105c80295c784c7c4911f155a5db8fd8529009
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-10-27 19:05:50 +0300

    Add Class Math to Homework6.2

---
+++ Homework6/Homework6.2/Homework6.2/Math.cs
@@ -0,0 +1,33 @@
+﻿namespace Homework6._2
+{
+    public class Math
+    {
+        public void Add(double firstNumber, double secondNumber)
+        {
+            double sum = firstNumber + secondNumber;
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {sum}");
+        }
+        public void Subtract(double firstNumber, double secondNumber)
+        {
+            double difference = firstNumber - secondNumber;
+            Console.WriteLine($"{firstNumber} - {secondNumber} = {difference}");
+        }
+        public void Multiply(double firstNumber, double secondNumber)
+        {
+            double product = firstNumber * secondNumber;
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {product}");
+        }
+        public void Divide(double firstNumber, double secondNumber)
+        {
+            if (secondNumber != 0)
+            {
+                double quotient = firstNumber / secondNumber;
+                Console.WriteLine($"{firstNumber} : {secondNumber} = {quotient}");
+            }
+            else
+            {
+                Console.WriteLine("Делить на нуль нельзя!");
+            }
+        }
+    }
+}
