0825f360ce2dd3a91004aa45a51da95368f00b09
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-10-29 22:26:22 +0300

    Add Homework 7.1

---
+++ Homerwork7/Homework7.1/Homework7.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using Homework7._1;
+using System.Text.RegularExpressions;
+string pattern = "(^[0-9]*[.,]?[0-9]*$)|(^[\\s]*$)";
+string radius;
+string x;
+string y;
+string z;
+
+do
+{
+    Console.Write("Введите радиус сферы: ");
+    radius = Console.ReadLine();
+}
+while (!Regex.IsMatch(radius, pattern));
+
+do
+{
+    Console.Write("Введите абсциссу х  центра сферы: ");
+    x = Console.ReadLine();
+}
+while (!Regex.IsMatch(x, pattern));
+
+do
+{
+    Console.Write("Введите ординату y центра сферы: ");
+    y = Console.ReadLine();
+}
+while (!Regex.IsMatch(y, pattern));
+
+do
+{
+    Console.Write("Введите аппликату z центра сферы: ");
+    z = Console.ReadLine();
+}
+while (!Regex.IsMatch(z, pattern));
+
+Sphere sphere = new Sphere(radius, x, y, z);
---
+++ Homerwork7/Homework7.1/Homework7.1/Sphere.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+namespace Homework7._1
+{
+    public class Sphere
+    {
+        public double Radius { get; set; } = 1;
+        private double _x = 0;
+        private double _y = 0;
+        private double _z = 0;
+        private double _distance;
+        private string _pattern = "^[\\s]*$";
+        public Sphere(string radius, string x, string y, string z)
+        {
+            if (!Regex.IsMatch(radius, _pattern))
+            {
+                Radius = double.Parse(radius, CultureInfo.InvariantCulture);
+            }
+
+            if (!Regex.IsMatch(x, _pattern))
+            {
+                _x = double.Parse(x, CultureInfo.InvariantCulture);
+            }
+
+            if (!Regex.IsMatch(y, _pattern))
+            {
+                _y = double.Parse(y, CultureInfo.InvariantCulture);
+            }
+
+            if (!Regex.IsMatch(z, _pattern))
+            {
+                _z = double.Parse(z, CultureInfo.InvariantCulture);
+            }
+        }
+
+        public double GetVolume()
+        {
+            return 4.0 / 3 * Math.PI * Radius * Radius * Radius;
+        }
+        public double GetSquare()
+        {
+            return 4 * Math.PI * Radius * Radius;
+        }
+        public string GetCenter()
+        {
+            return $"Координаты центра сферы: ({_x}, {_y}, {_z})";
+        }
+        public void SetCenter(double x, double y, double z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+        public bool IsPointInside(double x, double y, double z)
+        {
+            _distance = Math.Sqrt((x - _x) * (x - _x) + (y - _y) * (y - _y) + (z - _z) * (z - _z));
+            return _distance < Radius;
+        }
+    }
+}
