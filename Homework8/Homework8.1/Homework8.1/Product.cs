2c9b9010bed3f780b17604b92d8ea37131b40edb
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-11-06 20:32:23 +0300

    Add Homework8.1

---
+++ Homework8/Homework8.1/Homework8.1/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace Homework8._1
+{
+    public class Product
+    {
+        public string ProductName { get; set; }
+        public string StoreName { get; set; }
+        public decimal Price { get; set; }
+
+        public string ProductInformation()
+        {
+            return $"Товар {ProductName}, магазин - '{StoreName}', стоимость - {Price} руб.";
+        }
+    }
+}
---
+++ Homework8/Homework8.1/Homework8.1/Stock.cs
@@ -0,0 +1,53 @@
+﻿namespace Homework8._1
+{
+    public class Stock
+    {
+        private List<Product> products;
+
+        public Stock()
+        {
+            products = new List<Product>();
+        }
+
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+
+        public void RemoveProduct(Product product)
+        {
+            products.Remove(product);
+        }
+
+        public string GetProductInfoByIndex(int index)
+        {
+            if (index >= 0 && index < products.Count)
+            {
+                return products[index].ProductInformation();
+            }
+            return "Продукта с таким индексом на складе нет!";
+        }
+
+        public string GetProductInfoByName(string productName)
+        {
+            foreach (var product in products)
+            {
+                if (product.ProductName == productName)
+                {
+                    return product.ProductInformation();
+                }
+            }
+            return "Продукта с таким названием на складе нет!";
+        }
+
+        public static decimal operator +(Stock stock)
+        {
+            decimal totalPrice = 0;
+            foreach (var product in stock.products)
+            {
+                totalPrice += product.Price;
+            }
+            return totalPrice;
+        }
+    }
+}
