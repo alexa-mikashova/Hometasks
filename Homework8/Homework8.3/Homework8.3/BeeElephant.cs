dbd1339098e4b5a684c4812a948539a5f9002515
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-11-06 20:37:33 +0300

    Add Homework8.3

---
+++ Homework8/Homework8.3/Homework8.3/BeeElephant.cs
@@ -0,0 +1,38 @@
+﻿namespace Homework8._3
+{
+    public class BeeElephant
+    {
+        private int _beePart;
+        private int _elephantPart;
+        public BeeElephant(int beePart, int elephantPart)
+        {
+            _beePart = beePart;
+            _elephantPart = elephantPart;
+        }
+
+        public bool Fly()
+        {
+            return _beePart >= _elephantPart;
+        }
+
+        public string Trumpet()
+        {
+            return _elephantPart >= _beePart ? "tu-tu-doo-doo" : "wzzzz";
+        }
+
+        public string Eat(string meal, int value)
+        {
+            switch (meal)
+            {
+                case "nectar":
+                    _elephantPart = Math.Max(0, _elephantPart - value);
+                    return $"Часть пчелы - {_beePart}, часть слона - {_elephantPart}";
+                case "grass":
+                    _beePart = Math.Min(100, _beePart + value);
+                    return $"Часть пчелы - {_beePart}, часть слона - {_elephantPart}";
+                default:
+                    return "meal is not nectar or grass";
+            }
+        }
+    }
+}
---
+++ Homework8/Homework8.3/Homework8.3/Program.cs
@@ -0,0 +1,7 @@
+﻿using Homework8._3;
+BeeElephant beeElephant = new(80, 100);
+Console.WriteLine(beeElephant.Fly());
+Console.WriteLine(beeElephant.Trumpet());
+Console.WriteLine(beeElephant.Eat("nectar", 56));
+Console.WriteLine(beeElephant.Eat("grass", 81));
+Console.WriteLine(beeElephant.Eat("nnn", 90));
