c801f2194f9d111c69992333b4679250d8f18539
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-11-08 19:31:33 +0300

    Add spaces between class members

--- Homework6/Homework6.2/Homework6.2/Math.cs
+++ Homework6/Homework6.2/Homework6.2/Math.cs
@@ -6,14 +6,17 @@
         {
             return firstNumber + secondNumber;
         }
+
         public double Subtract(double firstNumber, double secondNumber)
         {
             return firstNumber - secondNumber;
         }
+
         public double Multiply(double firstNumber, double secondNumber)
         {
             return firstNumber * secondNumber;
         }
+
         public double Divide(double firstNumber, double secondNumber)
         {
             return firstNumber / secondNumber;
