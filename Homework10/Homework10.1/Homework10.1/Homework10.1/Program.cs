575a0acb55945b3b8ae974b53a3abf267c9ae3bb
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-12-10 22:42:32 +0300

    Add Homework10.1

---
+++ Homework10/Homework10.1/Homework10.1/Homework10.1/Program.cs
@@ -0,0 +1,41 @@
+﻿using (StreamReader input = new StreamReader("../../../../input_text.txt"))
+using (StreamWriter output = new StreamWriter("../../../../output_text.txt"))
+{
+    char[] characterCeparator = { ' ', ',', '.', ':', ';', '-', '!', '?', '\t', '\n', '\r' };
+    string line;
+    string lineToLover;
+    while ((line = input.ReadLine()) != null)
+    {
+        if (String.IsNullOrWhiteSpace(line))
+        {
+            continue;
+        }
+        lineToLover = line.ToLower();
+        var words = lineToLover.Split(characterCeparator);
+        var countWord = new Dictionary<string, int>();
+        foreach (var word in words)
+        {
+            if (countWord.ContainsKey(word))
+            {
+                countWord[word]++;
+            }
+            else
+            {
+                countWord[word] = 1;
+            }
+        }
+
+        string mostCommonWord = null;
+        int maxCount = 0;
+        foreach (var pair in countWord)
+        {
+            if (pair.Value > maxCount)
+            {
+                mostCommonWord = pair.Key;
+                maxCount = pair.Value;
+            }
+        }
+
+        output.WriteLine($"{mostCommonWord} {maxCount}");
+    }
+}
