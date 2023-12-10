c69193318eeb07d4a5a0da950c451f50efb19b3d
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-12-10 22:43:18 +0300

    Add Homework10.2

---
+++ Homework10/Homework10.2/Homework10.2/Homework10.2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+string uncensoredTextFile = @"E:\\Courses\\Hometasks\\Homework10\\Homework10.2\\uncensored_text.txt";
+string text = File.ReadAllText(uncensoredTextFile).ToLower();
+string forbiddenWordsFile = @"E:\\Courses\\Hometasks\\Homework10\\Homework10.2\\forbidden_words.txt";
+string forbiddenWords = File.ReadAllText(forbiddenWordsFile);
+string[] forbiddenWordsArray = forbiddenWords.Split(' ');
+foreach (string word in forbiddenWordsArray)
+{
+    string pattern = word;
+    text = Regex.Replace(text, pattern, new string('*', word.Length));
+}
+
+Console.WriteLine(text);
