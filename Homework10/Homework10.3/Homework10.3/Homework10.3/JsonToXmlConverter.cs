6659557164fcf1f647a265a1ffffe3bb8a2ed772
Alexandra Mikashova <alexa.mikashova@gmail.com>
2023-12-10 22:41:53 +0300

    Add Homework10.3

---
+++ Homework10/Homework10.3/Homework10.3/Homework10.3/JsonToXmlConverter.cs
@@ -0,0 +1,82 @@
+﻿using System.Text.Json;
+using System.Text;
+using System.Xml.Serialization;
+using System.Xml;
+
+namespace Homework10._3
+{
+    public class Person
+    {
+        public string Name { get; set; }
+
+        public string Birthday { get; set; }
+
+        public int Height { get; set; }
+
+        public double Weight { get; set; }
+
+        public bool Car { get; set; }
+
+        public List<string> Languages { get; set; }
+    }
+
+    public class JsonToXmlConverter
+    {
+        private List<Person> people;
+
+        public string ConvertJsonToXml(string jsonFilePath)
+        {
+            string json = File.ReadAllText(jsonFilePath);
+            people = JsonSerializer.Deserialize<List<Person>>(json)!;
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Person>));
+            StringBuilder xml = new StringBuilder();
+            using (XmlWriter writer = XmlWriter.Create(xml))
+            {
+                serializer.Serialize(writer, people);
+            }
+            return xml.ToString();
+        }
+
+        public string SaveToXmlFile(string xmlString, string xmlFilePath)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlString);
+            xmlDoc.Save(xmlFilePath);
+            return $"Данные сохранены в файл XML по пути: {xmlFilePath}";
+        }
+
+        public string PrintPersonData(string name)
+        {
+            Person person = people.Find(p => p.Name == name)!;
+            if (person != null)
+            {
+                return $"Name: {person.Name};\nBirthday: {person.Birthday};\nHeight: {person.Height};\nWeight: {person.Weight};\nCar: {person.Car};\nLanguages: {string.Join(", ", person.Languages)};";
+            }
+            else
+            {
+                return "Человек с данным именем не найден.";
+            }
+        }
+
+        public string FilterPeopleLanguage(string language)
+        {
+            List<string> names = new List<string>();
+            foreach (Person person in people)
+            {
+                if (person.Languages.Contains(language))
+                {
+                    names.Add(person.Name);
+                }
+            }
+
+            if (names.Count > 0)
+            {
+                return $"Имена людей владеющие языком {language}: {string.Join(", ", names)}";
+            }
+            else
+            {
+                return $"Человек, владеющий языком {language} не найден.";
+            }
+        }
+    }
+}
---
+++ Homework10/Homework10.3/Homework10.3/Homework10.3/Program.cs
@@ -0,0 +1,54 @@
+﻿using Homework10._3;
+JsonToXmlConverter converter = new JsonToXmlConverter();
+string choice;
+string xml;
+while (true)
+{
+    Console.WriteLine();
+    Console.WriteLine("Выберите нужное Вам действие:");
+    Console.WriteLine("1. Конвертация JSON => XML");
+    Console.WriteLine("2. Выход из программы");
+    choice = Console.ReadLine();
+    switch (choice)
+    {
+        case "1":
+            xml = converter.ConvertJsonToXml("../../../../../employees.json");
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Выберите нужное Вам действие:");
+                Console.WriteLine("1. Сохранить данные в XML-файл");
+                Console.WriteLine("2. Получить информацию о человеке по его имени");
+                Console.WriteLine("3. Применить к сотрудникам фильтр по изученным языкам");
+                Console.WriteLine("4. Выход из программы");
+                choice = Console.ReadLine();
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine(converter.SaveToXmlFile(xml, "../../../../../saveXml.xml"));
+                        break;
+                    case "2":
+                        Console.Write("Введите имя сотрудника:");
+                        string employeeName = Console.ReadLine();
+                        Console.WriteLine(converter.PrintPersonData(employeeName));
+                        break;
+                    case "3":
+                        Console.Write("Введите язык программирования для фильтрации:");
+                        string programmingLanguage = Console.ReadLine();
+                        Console.WriteLine(converter.FilterPeopleLanguage(programmingLanguage));
+                        break;
+                    case "4":
+                        return;
+                    default:
+                        Console.WriteLine("Повторите ввод снова!");
+                        break;
+                }
+            }
+            break;
+        case "2":
+            return;
+        default:
+            Console.WriteLine("Повторите ввод снова!");
+            break;
+    }
+}
